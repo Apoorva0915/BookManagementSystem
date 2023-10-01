bb3e140c59e7ad79f7e8ce12094a72b1db11f4a4
Apoorva Kesarwani <129680963+Apoorva0915@users.noreply.github.com>
2023-10-01 12:54:11 +0530

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    "mongodb+srv://apoorvakesarwani001:guPToyD73t8Dzskv@cluster0.2x5ozmg.mongodb.net/bookStore"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
